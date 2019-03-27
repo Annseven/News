2863b011e60899445945d9464bdeb34539f4c254
‘e <1286270208@qq.com>
2019-03-27 18:52:52 +0800

    1、增加Ioc练习的方法

---
+++ src/main/java/com/nowcoder/service/TouTiaoservice.java
@@ -0,0 +1,16 @@
+package com.nowcoder.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: AnNing
+ * @Description:
+ * @Date: Create in 17:52 2019/3/27
+ */
+@Service
+public class TouTiaoservice {
+
+    public String say(){
+        return "这是一个Service...";
+    }
+}
